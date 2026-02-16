ff0012b4331cb1039ddccee91b09d04de25497e2
Dhruv Shah <dhruvkshah75@gmail.com>
2026-02-16 15:21:21 +0530

    PYQ: Solved the Q1 Lab5 PYQ

--- PYQs/fact_temp.asm
+++ PYQs/fact_temp.asm
@@ -1,5 +1,8 @@
+; A program for Q1 PYQ Lab5 Eval  
+
 .model small
 .stack 100h
+
 .data
     newline db 0dh, 0ah, '$'  ; newline characters
     num db ?
@@ -10,7 +13,78 @@ main proc
 	;Initialize data segment
     mov ax, @data
     mov ds, ax
-   
 
+    ; we can take the input of a 2 digit number as taking input of two characters 
+    ; the first inputed number * 10 + second number 
+
+    ; NOTE: For 8 bit multiplication => AX = AL * Operand 
+
+    ; ---- Get FIRST Digit ----
+    mov ah, 01h
+    int 21h            ; input is stored in AL
+    mov bl, al 
+    sub bl, '0'
+
+    ; ---- GET SECOND Digit ----
+    xor ax, ax 
+    mov ah, 01h
+    int 21h
+    mov cl, al 
+    sub cl, '0'
+
+    mov al, bl          
+    mov ch, 10 
+    mul ch              ; Now AX store the val of first char * 10
+    xor ch, ch
+    add ax, cx          ; cx => 00cl we must make ch 0 for 
+
+    mov num, al         ; Finally store the number into num variable => note: the max val is 99(decimal) can be stored in db
+                        ; So AX holds 00AL => the number is stored in AL 
+
+    ; we can just run a loop from 1 to num and then check if the remainder division is 0 
+ 
+    lea dx, newline     ; now we move to a newline
+    mov ah, 09h
+    int 21h
+
+    xor ax, ax
+    xor bx, bx
+    mov bl, 1            ; bl => i = 1 
+
+    mov dx, 0            ; we maintain the current sum => can exceed 8 bits 
+
+factor_loop:
+    xor ax, ax
+    mov al, num  
+    ; now ax holds the number => divident is 16 bit 
+    ; now on division by bl we get AL => Quotient, AH => Remainder 
+    div bl              ; we divide the num in ax by bl (i)
+    cmp ah, 0    
+    jne skip_factor
+    add dx, bx          ; if rem = 0 then add (i) bl to dx => dx = dx + bx (as bx = 00bl)
+    
+skip_factor:
+    inc bl              ; now we increment bl 
+    cmp bl, num 
+    jbe factor_loop     ; continue if bl <= num
+
+
+    ; now we have to print the modulo 10 of the sum => divide by 10 
+    xor ax, ax
+    mov ax, dx  
+    xor bl, bl
+    mov bl, 10
+    div bl              ; now AH stores the modulo 10 of the sum of factors 
+
+    mov dl, ah 
+    add dl, '0'         ; convert to ASCII
+    xor ax, ax
+    mov ah, 02h
+    int 21h
+
+exit_program:
+    mov ah, 4ch
+    int 21h
+   
 main endp
 end main
--- bubbleSort.asm
+++ bubbleSort.asm
@@ -4,8 +4,9 @@
 .stack 100h
 
 .data
-    newline 13, 10, '$'
+    newline db 13, 10, '$'
     array db 50 dup(0)         ; array that we will take input of (input is taken char by char)
+    len db ?                   ; store the len of the array 
 
 .code 
 main proc 
@@ -15,11 +16,13 @@ main proc
 
     xor ax, ax
     mov ah, 01h
-    int 21h                    ; take the input of the length of the array in AL register
+    int 21h                    ; take the input of the length of the array in AL register (ASCII)
+    sub al, '0'
 
-    mov cl, al                 ; store the length in cl
 
-    mov bl, cl                 ; store the length in the bl for the input loop
+    mov len, al                ; store the length in len  
+
+    mov cl, len                ; store the length in the cl for the input loop
     lea si, array              ; pointer of the array 
 
 input_loop:
@@ -27,7 +30,91 @@ input_loop:
     mov ah, 01h
     int 21h                    ; element of the array is stored in the AL register 
 
-    
+    ; ----- FILTERING -----
+    cmp al, ' '
+    je input_loop              ; skip this element if a white space is inputed 
+
+    cmp al, 13                 ; Check for ENTER key (CR)
+    je input_loop              ; If Enter, skip it too!
+
+    sub al, '0'                ; convert the number from ASCII back to a number 
+
+    mov [si], al               ; store the element in array
+    inc si
+    dec cl
+    cmp cl, 0
+    jne input_loop
+
+
+bubble_sort:                  ; Now we sort the inputed array
+    xor si, si               
+    mov si, 0                 ; i = 0 => si = i
+
+outer_loop:
+    xor di, di                
+    mov di, 0                 ; j = 0 => di = j
+
+inner_loop:                   ; the inner loop runs from j = 0 to j < len - i - 1
+    mov bh, array[di]
+    mov dh, array[di+1]          
+    cmp bh, dh                ; if a[i] > a[j] then swap
+    jbe skip_swap
+    mov array[di], dh         ; now we swap the elements 
+    mov array[di+1], bh 
+
+skip_swap:
+    inc di
+    xor bx, bx
+    mov bl, len               ; bx now stores 00len   
+    sub bx, si
+    dec bx                    ; now bx stores len - i - 1 (bl = len - si - 1)
+    cmp di, bx                ; cmp j, len - i - 1
+    jb inner_loop
+
+    inc si
+    xor cx, cx
+    mov cl, len
+    cmp si, cx                ; cmp i < len(cx holds the value of len)
+    jb outer_loop
+
+
+print_result: 
+    xor si, si
+    lea si, array
+    xor cx, cx
+    mov cl, len
+
+    lea dx, newline          ; print a newline 
+    mov ah, 09h
+    int 21h
+
+print_loop:
+    xor dx, dx
+    mov dl, [si]
+    mov ah, 02h
+    add dl, '0'               ; convert to ASCII
+    int 21h
+
+    mov dl, ' '
+    int 21h                   ; print the space
+
+    inc si
+    dec cl
+    cmp cl, 0
+    jne print_loop 
+
+exit_program:
+    mov ah, 4ch
+    int 21h
+
+main endp
+end
+
+
+
+
+
+
 
 
 
